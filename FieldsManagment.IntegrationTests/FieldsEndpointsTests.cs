4b139f29b89649a1d21950b0d45e447cbdbef430
bartosz.korga@rockwool.com <bartosz.korga@rockwool.com>
2024-03-07 16:54:39 +0100

    add

---
+++ FieldsManagment.IntegrationTests/FieldsEndpointsTests.cs
@@ -0,0 +1,25 @@
+﻿using FieldsManagment.IntegrationTests;
+using System;
+using Xunit.Abstractions;
+
+namespace FieldsManagement.IntegrationTests;
+
+public class FieldsEndpointsTests : IClassFixture<FieldsManagementWebAplicationFactory>
+{
+    private readonly FieldsManagementWebAplicationFactory _webAppFactory;
+
+    public FieldsEndpointsTests(FieldsManagementWebAplicationFactory webAppFactory, ITestOutputHelper testOutputHelper)
+    {
+        _webAppFactory = webAppFactory;
+        _webAppFactory.Output = testOutputHelper;
+    }
+
+    [Fact]
+    public async Task CRUDTests()
+    {
+        var client = _webAppFactory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("");
+    }
+}
---
+++ FieldsManagment.IntegrationTests/FieldsManagementWebAplicationFactory.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Testcontainers.MongoDb;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace FieldsManagment.IntegrationTests;
+
+public class FieldsManagementWebAplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    public ITestOutputHelper? Output { get; set; }
+    private readonly MongoDbContainer _conteiner = new MongoDbBuilder().Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            if (Output != null)
+            {
+                logging.SetMinimumLevel(LogLevel.Error);
+            }
+        });
+
+        builder.ConfigureAppConfiguration(configuration =>
+        {
+            var connectionString = _conteiner.GetConnectionString();
+
+            configuration.AddInMemoryCollection(new Dictionary<string, string?>()
+            {
+                { "ConnectionStrings:MongoDb", connectionString },
+                { "MongoDb:DatabaseName", "corerock_tests" }
+            });
+        });
+    }
+
+    async Task IAsyncLifetime.InitializeAsync()
+    {
+        await _conteiner.StartAsync();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await _conteiner.StopAsync();
+        await _conteiner.DisposeAsync();
+    }
+}
