d0dbb8e9da7148e7ca64bc2ffea8d00b43a58460
bartosz.korga@rockwool.com <bartosz.korga@rockwool.com>
2024-03-28 18:50:15 +0100

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { ChakraProvider } from '@chakra-ui/react'
+import AnimationComponent from "./Components/AnimationComponent";
+import LoginComponent from "./Components/LoginComponent"; // Załóżmy, że taki komponent istnieje
+import background from "./Components/styl/farmland.jpg";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import useApiHook from "./Hooks/useApiHook";
-import "./App.css";
-import { get } from "http";
-
-type Field = {
-  id: string;
-  villageName: string;
-  area: number;
-  additionalData: string;
-};
 
 const App: React.FC = () => {
-  const { data, loading, error } = useApiHook<Field[]>(
-    "https://localhost:7138/Fields",
-    "GET"
+  const [showLogin, setShowLogin] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLogin(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  return (
+    <div style={{backgroundImage: `url(${background})`, backgroundSize: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <div style={{ width: '80%', backgroundColor: "rgba(30,30,30, 0.6)", display: 'flex', justifyContent: 'center' }}>
+            {showLogin ? <LoginComponent /> : <AnimationComponent />}
+          </div>
+        </ChakraProvider>
+      </QueryClientProvider>
+    </div>
   );
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  if (data) {
-    return (
-      <div>
-        {data.map((field) => (
-        <div key={field.id}>
-          <h3>{field.villageName}</h3>
-          <p>{field.area}</p>
-          <p>{field.additionalData}</p>
-        </div>
-))}
-
-      </div>
-    );
-  }
-
-  return null;
+  
+  
+  
+  
 };
 
 export default App;
