77d85cf0f92a25d8deca49e6f67e8f4da2a81d74
bartosz.korga@rockwool.com <bartosz.korga@rockwool.com>
2024-04-07 18:14:27 +0200

    add new pages

---
+++ client/src/Pages/FieldsPage.tsx
@@ -0,0 +1,3 @@
+export default function FieldsPage() {
+  return <div className="flex flex-col gap-2">fields page</div>;
+}
--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -12,17 +12,17 @@ const HomePage = () => (
       gap={4}
     >
       <GridItem rowSpan={2} colSpan={2}>
-        <Link href="/page1">
+        <Link href="/Operations">
           <ChooseBoxComponent name="Dodaj Operacje" background="black" />
         </Link>
       </GridItem>
       <GridItem rowSpan={2} colSpan={1}>
-        <Link href="/page2">
+        <Link href="/Fields">
           <ChooseBoxComponent name="Stwórz Pole" background="green" />
         </Link>
       </GridItem>
       <GridItem rowSpan={1} colSpan={2}>
-        <Link href="/page3">
+        <Link href="/Workers">
           <ChooseBoxComponent
             name="Stwórz konto dla pracownika"
             background="green"
@@ -30,7 +30,7 @@ const HomePage = () => (
         </Link>
       </GridItem>
       <GridItem rowSpan={1} colSpan={1}>
-        <Link href="/page4">
+        <Link href="/Statistics">
           <ChooseBoxComponent name="Statystyki" background="black" />
         </Link>
       </GridItem>
