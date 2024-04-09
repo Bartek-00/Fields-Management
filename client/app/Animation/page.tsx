15d760c1a4bcec94dfb38b9683b72940013a890b
bartosz.korga@rockwool.com <bartosz.korga@rockwool.com>
2024-04-09 19:43:57 +0200

    change animation page

--- client/app/Animation/page.tsx
+++ client/app/Animation/page.tsx
@@ -3,16 +3,21 @@ import React, { useEffect } from "react";
 import styles from "./AnimationComponent.module.css";
 import { redirect } from "next/dist/server/api-utils";
 import Link from "next/link";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 const page = () => {
   return (
     <div className={styles["fullscreen-bg"]}>
       <div className={styles["background-image"]} />
       <div className={styles.overlay}>
-        <h1>Tekst Animowany</h1>
-        <Link href="/Home" replace>
-          cokowliek
-        </Link>
+        <Box display="flex" flexDirection="column" padding="5rem">
+          <Text fontSize="4xl" color="white">
+            Wejdź na nowy poziom zarządzania gospodarstwem
+          </Text>
+          <Button as="a" href="/LoginPage" colorScheme="green" size="lg">
+            Zaloguj się aby zacząć korzystać z aplikacji
+          </Button>
+        </Box>
       </div>
     </div>
   );
